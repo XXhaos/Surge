abaa94d0e97185cb445dcf12ba3a534cf0f6271c
XXhaos <53092039+XXhaos@users.noreply.github.com>
2024-11-23 01:48:37 +0800

    Update XBOX_NG.js

--- XBOX_NG.js
+++ XBOX_NG.js
@@ -1,12 +1,7 @@
 (function() {
-  // 解析请求体为 JSON
   let body = JSON.parse($request.body);
-
-  // 修改 JSON 数据
   body.market = "NG";
   body.locale = "en-NG";
   body.friendlyName = "cart-NG";
-
-  // 返回修改后的请求体
   $done({ body: JSON.stringify(body) });
 })();
