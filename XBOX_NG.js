9d3128bb6374f213b656bd802e539f104d5d623e
XXhaos <53092039+XXhaos@users.noreply.github.com>
2024-11-23 11:39:43 +0800

    Update XBOX_NG.js

--- XBOX_NG.js
+++ XBOX_NG.js
@@ -1,9 +1,9 @@
 let body = JSON.parse($request.body);
 
-// 修改请求体的相应字段为尼日利亚
+// 修改为尼日利亚的字段
 body.market = "NG";
 body.locale = "en-NG";
 body.friendlyName = "cart-NG";
 
-// 将修改后的对象转换回字符串并返回
-$done({body: JSON.stringify(body)});
+// 返回修改后的请求体
+$done({ body: JSON.stringify(body) });
