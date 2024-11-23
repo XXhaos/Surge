75c7523dc0eff3144676329a7221b335b00e93d9
XXhaos <53092039+XXhaos@users.noreply.github.com>
2024-11-23 11:28:26 +0800

    Update XBOX_NG.js

--- XBOX_NG.js
+++ XBOX_NG.js
@@ -1,15 +1,9 @@
-const $ = new Env('MS Cart Region');
-let body = JSON.parse($response.body);
+let body = JSON.parse($request.body);
 
+// 修改请求体的相应字段为尼日利亚
 body.market = "NG";
 body.locale = "en-NG";
 body.friendlyName = "cart-NG";
 
-$done({ 
-    body: JSON.stringify(body),
-    headers: $response.headers,
-    status: $response.status
-});
-
-function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,a)=>{s.call(this,t,(t,s,r)=>{t?a(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",Object.assign(this,e)}};
-}
+// 将修改后的对象转换回字符串并返回
+$done({body: JSON.stringify(body)});
