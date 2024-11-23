9940418ea0976559a690b8fdcb42b03a563e35e0
XXhaos <53092039+XXhaos@users.noreply.github.com>
2024-11-23 10:35:56 +0800

    Update XBOX_NG.js

--- XBOX_NG.js
+++ XBOX_NG.js
@@ -1,26 +1,8 @@
-(function() {
-  const version = 'V1.0.0';
-  const market = 'NG';
-  const locale = 'en-NG';
-  const friendlyName = 'cart-NG';
+let body = $request.body;
 
-  try {
-    let body = JSON.parse($request.body);
-    
-    // 打印原始请求体用于调试
-    console.log(`[${version}] Original Request Body:`, body);
+// 使用正则替换特定的字段值
+body = body.replace(/\"market\":\"[^\"]+\"/g, '\"market\":\"NG\"');
+body = body.replace(/\"locale\":\"[^\"]+\"/g, '\"locale\":\"en-NG\"');
+body = body.replace(/\"friendlyName\":\"[^\"]+\"/g, '\"friendlyName\":\"cart-NG\"');
 
-    // 修改字段
-    body.market = market;
-    body.locale = locale;
-    body.friendlyName = friendlyName;
-
-    // 打印修改后的请求体用于调试
-    console.log(`[${version}] Modified Request Body:`, body);
-
-    $done({ body: JSON.stringify(body) });
-  } catch (error) {
-    console.error(`[${version}] Error modifying request body:`, error);
-    $done({});
-  }
-})();
+$done({ body });
