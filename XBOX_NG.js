22a62d9c817af977935ae95fe3eb767df7b9a81a
XXhaos <53092039+XXhaos@users.noreply.github.com>
2024-11-23 10:57:21 +0800

    Update XBOX_NG.js

--- XBOX_NG.js
+++ XBOX_NG.js
@@ -1,14 +1,19 @@
-let body = $request.body;
+let body = $response.body;
 
-if ($request.url.includes("/v1.0/cart/loadCart")) {
-    body = JSON.parse(body);
-
-    // 修改请求体中的相关字段
-    body.market = "NG";
-    body.locale = "en-NG";
-    body.friendlyName = "cart-NG";
-
-    body = JSON.stringify(body);
+if ($request.url.match(/https:\/\/cart\.production\.store-web\.dynamics\.com\/v1\.0\/cart\/loadCart.*/)) {
+    // 将响应体解析为JSON对象
+    let obj = JSON.parse(body);
+    
+    // 使用jq语法修改对象属性
+    obj.market = "NG";
+    obj.locale = "en-NG";
+    obj.friendlyName = "cart-NG";
+    
+    // 将修改后的对象转回字符串
+    body = JSON.stringify(obj);
 }
 
-$done({ body: body });
+$done({
+    status: 200,
+    body: body
+});
