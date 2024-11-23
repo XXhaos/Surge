1212351c9b4fc268d18f063202113e072ae34def
XXhaos <53092039+XXhaos@users.noreply.github.com>
2024-11-23 11:58:25 +0800

    Update XBOX_NG.js

--- XBOX_NG.js
+++ XBOX_NG.js
@@ -1,14 +1,14 @@
-let body = JSON.parse($response.body);
+let body = JSON.parse($request.body);
 
+// 打印日志查看请求是否正确被捕获
+console.log("Original Body:", JSON.stringify(body));
+
+// 修改市场、地区等字段为尼日利亚
 body.market = "NG";
 body.locale = "en-NG";
 body.friendlyName = "cart-NG";
 
-$done({ 
-    body: JSON.stringify(body),
-    headers: $response.headers,
-    status: $response.status
-});
+// 打印修改后的body
+console.log("Modified Body:", JSON.stringify(body));
 
-function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,a)=>{s.call(this,t,(t,s,r)=>{t?a(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",Object.assign(this,e)}};
-}
+$done({ body: JSON.stringify(body) });
