947084c66170fc67d269e8ed6185c46b84eaa6ff
XXhaos <dragonisheep@gmail.com>
2026-04-18 13:15:58 +0800

    Update capture_gamertag.js

--- Scripts/capture_gamertag.js
+++ Scripts/capture_gamertag.js
@@ -19,7 +19,7 @@
 const peoplePattern = /^https:\/\/peoplehub-public\.xboxlive\.com\/people\/gt\(.+\)/;
 const url = $request.url;
 
-const MAX_RECORDS = 20;   // gamertag 记录保留最近 20 条
+const MAX_RECORDS = 30;   // gamertag 记录保留最近 30 条
 
 if (peoplePattern.test(url)) {
     if (!$response.body) {
